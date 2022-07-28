5f49456ec2bab5618c65eaba8f578f041040db37
sibramadoss <ssr126@scarletmail.rutgers.edu>
2022-07-28 11:39:15 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print('Hello, world!')
+class A(object):
+    def __init__(self, data = None):
+        self.data = data
+
+    def method_a(self, foo):
+        print(str(self.data) + ' ' + foo)
+
+if __name__ == '__main__':
+    a = A(100)
+    a.method_a('sailor!')
