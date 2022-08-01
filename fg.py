88e7e368894aeb361eeada7db78ceb4ae3f01551
sibramadoss <ssr126@scarletmail.rutgers.edu>
2022-07-31 22:57:58 -0400

    Comments index == index+5

--- fg.py
+++ fg.py
@@ -61,24 +61,20 @@ while len(SolutionArr) < 101:
         arr711.remove((tempMin_711))
 #print(SolutionArr)
 
+#14 --> 13 
 red = [0, 2, 4, 5, 6, 7, 10, 11, 12, 13, 14, 15, 18, 20]
+print(len(red))
+print(range(0,9))
 
-print(red[7] == red[8] - 1)
-print(red[7])
-print(range(0,len(red)-1))
+for index in range(0,len(red) - 5):
+    if red[index] != red[index + 5] - 5:
+        continue
+    elif red[index] == red[index + 5] - 5:
+        print(index)
+        continue
+    else:
+        continue
 
-ctr = 0
-for index in range(0,len(red)-1):
-    if red[index] != red[index + 1] - 1:
-        ctr = 0
-        continue
-    elif red[index] == red[index+1] -1 : 
-        ctr = ctr + 1
-        if ctr == 6:
-            print(index - 6)
-            break
-        continue
-print(ctr)
 
 
 
