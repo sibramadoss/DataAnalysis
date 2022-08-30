35ac301cb04d875a7eb7015d5aab7718b7f23b18
sibramadoss <ssr126@scarletmail.rutgers.edu>
2022-08-30 16:07:46 +0530

    Update primenum.py

--- primenum.py
+++ primenum.py
@@ -1,4 +1,14 @@
-print('hello')
+import sympy
 
+i = 0
+res = []
 
-        
+while i>=0:
+    if sympy.isprime(i) == True:
+        res.append(i)
+    if len(res) == 20:
+        break
+    i += 1
+
+print(res)
+
