3dd56c6efe4830cc1836f04a5edbd4a0b03c8cbd
sibramadoss <ssr126@scarletmail.rutgers.edu>
2022-08-30 18:30:02 +0530

    prime_num total

---
+++ prime_num/while_pn.py
@@ -0,0 +1,23 @@
+i = 0
+pnlist = []
+
+while i >= 0:
+    if i == 0 or i == 1:
+        i += 1
+        continue
+    else:
+        for j in range(2, int(i/2)+1):
+            if i % j == 0:
+                break
+        else:
+            pnlist.append(i)
+            if len(pnlist) == 20:
+                break
+                #for else syntax --> the else block only executes if the for loop above does not break
+                #if the statement does break it skips to the i += 1 line and goes back to the while
+                #if the statement breaks it goes to pnlist.append and then checks is list == 20 
+                #if it satisfies it breaks out of the entroe while loop now
+        i += 1
+
+print(pnlist)
+
