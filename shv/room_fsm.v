d7a438771b02dafde47dab36783b007ba627cc29
sibramadoss <ssr126@scarletmail.rutgers.edu>
2022-10-10 22:26:11 -0400

    Update room_fsm.v

--- shv/room_fsm.v
+++ shv/room_fsm.v
@@ -4,24 +4,24 @@ module room_fsm (
 );
 
     wire s01;
-    assign s01 = (s1 & w & ~reset) | (reset);
+    assign s01 = (s1 & w & ~reset) | (reset) | (~reset & ~n & ~s & ~e & ~w & s01) | (s01 & n & ~reset) | (s01 & s & ~reset) | (s01 & w & ~reset) | (s01 & n & w & ~reset) | (s01 & s & w & ~reset) | (s01 & n & e & ~reset) | (s01 & s & e & ~reset);
     dff(clk, s01, s0);
 
     wire s11;
-    assign s11 = (s0 & e & ~reset) | (s2 & n & ~reset) | (s11 & ~reset);
+    assign s11 = (s0 & e & ~reset) | (s2 & n & ~reset) | (s11 & ~reset) | (~reset & ~n & ~s & ~e & ~w & s11) | (s11 & n & ~reset) | (s11 & e & ~reset) | (s11 & n & w & ~reset) | (s11 & s & w & ~reset) | (s11 & n & e & ~reset) | (s11 & s & e & ~reset);
     dff(clk, s11, s1);
 
     wire s21;
-    assign s21 = (s1 & s & ~reset) | (s3 & e & ~reset) | (s21 & ~reset);
+    assign s21 = (s1 & s & ~reset) | (s3 & e & ~reset) | (s21 & ~reset) | (~reset & ~n & ~s & ~e & ~w & s21) | (s21 & s & ~reset) | (s21 & e & ~reset) | (s21 & n & w & ~reset) | (s21 & s & w & ~reset) | (s21 & n & e & ~reset);
     dff(clk, s21, s2);
 
     wire s31;
-    assign s31 = (s2 & w & ~reset) | (s31 & ~reset);
+    assign s31 = (s2 & w & ~reset) | (s31 & ~reset) | (~reset & ~n & ~s & ~e & ~w & s31) | (s31 & n & ~reset) | (s31 & s & ~reset) | (s31 & w & ~reset)  | (s31 & w & ~reset) | (s31 & n & w & ~reset) | (s01 & s & w & ~reset) | (s31 & n & e & ~reset) | (s31 & s & e & ~reset);
     assign sw = s3;
     dff(clk, s31, s3);
 
     wire s41;
-    assign s41 = (s2 & (s & e) & ~reset) | (s41 & ~reset);
+    assign s41 = (s2 & s & e & ~reset) | (s41 & ~reset);
     dff(clk, s41, s4);
 
     wire s51;
