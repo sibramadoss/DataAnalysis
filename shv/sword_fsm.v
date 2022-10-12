d089e7b14de21d2c7ca75e277f9347e4f1b528bf
sibramadoss <ssr126@scarletmail.rutgers.edu>
2022-10-11 21:26:15 -0400

    Update sword_fsm.v

--- shv/sword_fsm.v
+++ shv/sword_fsm.v
@@ -2,12 +2,8 @@ module sword_fsm (
     input logic clk, reset, sw,
     output logic v
 );
-    //wire s10;
-    //assign s10 = (reset)
-    //dff(clk, s10, s0)
-
     wire s12;
-    assign s12 = ((sw | s12) & ~reset) | (s12 & ~reset & ~sw);
+    assign s12 = ((sw | v) & ~reset) | (v & ~reset & ~sw);
     dff(clk, s12, v);
 
 endmodule
