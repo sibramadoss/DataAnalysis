6e2ac0d8251e30e25341b1b35e70a43b9ec32004
sibramadoss <ssr126@scarletmail.rutgers.edu>
2022-10-14 12:31:21 -0400

    Update swordf_fsm.v

--- shv/swordf_fsm.v
+++ shv/swordf_fsm.v
@@ -5,11 +5,11 @@ module sword_fsm (
     
     wire s00;
     assign s00 = reset | (~reset & ~sw & s0);
-    lab04_sr ns (s00, clk, s0);
+    lab04_sr ns (clk, s00, s0);
 
     wire s01;
     assign s01 = (~reset & sw & s0) | (~reset & s1);
-    lab04_sr hs (s01, clk, s1);
+    lab04_sr hs (clk, s01, s1);
 
     assign v = s1;
 
