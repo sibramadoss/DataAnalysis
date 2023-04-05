1e337360f451142f49032e38722684b1af8ac841
sibramadoss <ssr126@scarletmail.rutgers.edu>
2023-04-05 15:17:05 -0400

    Python Venv Experiment

--- seniorproject.py
+++ seniorproject.py
@@ -1,1 +1,10 @@
-print("hello world!")
+from selenium import webdriver
+
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+browser = webdriver.Chrome('drivers/chromedriver.exe', chrome_options=chrome_options)
+browser.get('http://yahoo.com/')
+
+
