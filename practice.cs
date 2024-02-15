a505bec921284186b3d1612b8ef58de7f7679781
sibramadoss <ssr126@scarletmail.rutgers.edu>
2024-02-14 19:18:24 -0500

    Create practice.cs

---
+++ practice.cs
@@ -0,0 +1,16 @@
+using System;
+
+class HelloWorld {
+    static void MyMethod(string fname) {
+    Console.WriteLine(fname + " Refsnes");
+    }
+
+    static void Main()
+    {
+        for (int i = 0; i<3; i++){
+            //Console.WriteLine(i);
+            MyMethod(Console.ReadLine());
+        }
+    }
+}
+
