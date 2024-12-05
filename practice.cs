4b0e7c194fec3e7f217f8e6c410a90704a7328f3
sibramadoss <ssr126@scarletmail.rutgers.edu>
2024-12-05 11:38:24 -0500

    Update practice.cs

--- practice.cs
+++ practice.cs
@@ -8,7 +8,7 @@ class HelloWorld {
     static void Main()
     {
         for (int i = 0; i<3; i++){
-            //Console.WriteLine(i);
+            Console.WriteLine(i);
             MyMethod(Console.ReadLine());
         }
     }
